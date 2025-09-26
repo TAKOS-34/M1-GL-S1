8a0f8e192fb093ce8c6fd4fd6d4f06a693b6481e
takos <elliot.durand@proton.me>
2025-09-26 15:22:56 +0200

    ALD TP1 Exo1 - 2

--- Architectures logicielles distribuées/TP1/server/src/main/java/org/test/rmi/server/main/Application.java
+++ Architectures logicielles distribuées/TP1/server/src/main/java/org/test/rmi/server/main/Application.java
@@ -1,7 +1,8 @@
 package org.test.rmi.server.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// Classe principale de l'application
+import java.util.Objects;
+
 @SpringBootApplication(scanBasePackages = {
         "org.test.rmi.server.impl",
         "org.test.rmi.server.config"
@@ -9,6 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class Application {
     public static void main(String[] args) {
+        String policyUrl = Objects.requireNonNull(Application.class.getClassLoader().getResource("security.policy")).toString();
+        System.setProperty("java.security.policy", policyUrl);
+        System.setSecurityManager(new SecurityManager());
+
         SpringApplication.run(Application.class, args);
     }
 }
